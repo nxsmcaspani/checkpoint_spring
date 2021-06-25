75a75da1a61d3e48e4557bc1842291d6a9108e54
Maxime Caspani <maxime.caspani@natixis.com>
2021-06-25 09:37:02 +0200

    Initial commit

---
+++ src/test/java/fr/wildcodeschool/checkpoint_spring/CheckpointSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.wildcodeschool.checkpoint_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckpointSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
